d2899213de2a8b9931a7ce1480e46ce2aed66f56
Conrad Taylor <conradwt@gmail.com>
2021-04-23 17:39:50 -0700

    Update the default gravatar image.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def profile_image(user, size = 100)
-    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
+    url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}&d=robohash"
     image_tag(url, alt: user.name, class: 'rounded-circle z-depth-2', 'data-holder-rendered': 'true')
   end
 end
