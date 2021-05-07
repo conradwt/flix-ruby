0e03d4c852fcf5c0ff5b64a0633416391b7b6416
Conrad Taylor <conradwt@gmail.com>
2021-05-07 10:44:44 -0700

    fix guard clause

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def require_signin
-    unless current_user
-      session[:intended_url] = request.url
-      redirect_to new_session_url, alert: 'Please sign in first!'
-    end
+    return if current_user
+
+    session[:intended_url] = request.url
+    redirect_to new_session_url, alert: 'Please sign in first!'
   end
 
   def require_admin
